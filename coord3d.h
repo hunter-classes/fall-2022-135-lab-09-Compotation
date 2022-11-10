5825a7deddd4d9ade90a1060863247b3220b3bd4
Compotation <46853874+Compotation@users.noreply.github.com>
2022-11-10 15:00:22 -0500

    overload << operator

--- coord3d.h
+++ coord3d.h
@@ -4,7 +4,12 @@ public:
   double x;
   double y;
   double z;
-  bool operator==(const Coord3D& otherCoord) const {
+
+  bool operator==(const Coord3D &otherCoord) const {
     return x == otherCoord.x && y == otherCoord.y && z == otherCoord.z;
+  }
+
+  friend std::ostream& operator<<(std::ostream &s, const Coord3D &point) {
+    return s << "(" << point.x << ", " << point.y << ", " << point.z << ")";
   }
 };

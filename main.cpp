b1f5b302946af2aeab9781cecce734adba01bb5e
Compotation <46853874+Compotation@users.noreply.github.com>
2022-11-10 15:04:15 -0500

    main

--- main.cpp
+++ main.cpp
@@ -5,5 +5,18 @@
 
 int main()
 {
+  Coord3D pointP1 = {10, 20, 30};
+  std::cout << length(&pointP1) << std::endl;
+  Coord3D pointP2 = {25, 25, 25};
+  std::cout << length(&pointP2) << std::endl;
+  Coord3D* farther = fartherFromOrigin(&pointP1, &pointP2);
+  std::cout << *farther << std::endl;
+
+  Coord3D vel1 = {2, 3, 2};
+  move(&pointP1, &vel1, 5);
+  std::cout << pointP1 << std::endl;
+
+  Coord3D* createdP = createCoord3D(2, 8, 10);
+  deleteCoord3D(createdP);
   return 0;
 }

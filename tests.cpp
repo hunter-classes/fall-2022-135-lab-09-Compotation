62a33760e98dbc91da1ef19ea232d335aad36961
Compotation <46853874+Compotation@users.noreply.github.com>
2022-11-10 11:45:03 -0500

    task b

--- tests.cpp
+++ tests.cpp
@@ -8,3 +8,13 @@ TEST_CASE("length") {
   Coord3D pointP2 = {25, 25, 25};
   CHECK_LE(length(&pointP2) - 43.301270189, 1e-5);
 }
+
+TEST_CASE("farther from origin") {
+  Coord3D pointP1 = {10, 20, 30};
+  Coord3D pointP2 = {25, 25, 25};
+  CHECK_LE(fartherFromOrigin(&pointP1, &pointP2), &pointP2);
+
+  Coord3D pointP3 = {10, 20, 30};
+  Coord3D pointP4 = {25, 20, 25};
+  CHECK_LE(fartherFromOrigin(&pointP3, &pointP4), &pointP3);
+}
